3b563a44e17b0c3312223d5358093972f394d5ae
barbaraest <barbara.esteves@thoughtworks.com>
2021-09-20 18:46:07 -0300

    Adiciona cenários de get por id com sucesso e nao existente

--- cypress/integration/Me Conta/Usuario/usuario-Id-GET.js
+++ cypress/integration/Me Conta/Usuario/usuario-Id-GET.js
@@ -1,7 +1,49 @@
 /// <reference types="Cypress">
 
-describe('', () => {
-    it('', () => {
+describe('Me Conta?', () => {
+    it('GET - Consulta usuário pelo id com sucesso', () => {
+        cy.request({
+            method: 'GET', 
+            url: 'https://me-conta-backend.herokuapp.com/usuario/4',
+            failOnStatusCode: false,
+            headers:{ 
+                "accept" : 'application/json',
+                "Content-Type" : 'application/json',
+            },
+            /*params:{
+                "{id}" : 4,
+            },*/
+        }).as('response')
 
+        cy.get('@response').then(res =>{
+            cy.expect(res.status).to.be.eq(200)
+            cy.expect(res.body.id).to.be.eq(4)
+            cy.expect(res.body.nome).to.be.eq("Maria Silva")
+            cy.expect(res.body.email).to.be.eq("emailRaul.Melo@gmail.com")
+            cy.expect(res.body.genero).to.be.eq("M")
+            cy.expect(res.body.UF).to.be.eq("AC")
+            cy.expect(res.body.cidade).to.be.eq("Acrelândia")
+            cy.expect(res.body.telefone).to.be.eq("(81) 91234-5678")
+            cy.expect(res.body.tipoUsuario).to.be.eq("ATENDENTE")
+        })
+    })
+
+    it('GET - Consulta usuário pelo id não existe', () => {
+        cy.request({
+            method: 'GET', 
+            url: 'https://me-conta-backend.herokuapp.com/usuario/9999',
+            failOnStatusCode: false,
+            headers:{ 
+                "accept" : 'application/json',
+                "Content-Type" : 'application/json',
+            },
+        }).as('response')
+
+        cy.get('@response').then(res =>{
+            cy.expect(res.status).to.be.eq(404)
+            //cy.expect(res.message).to.be.eq("Não encontrado")
+            //cy.expect(res.error).to.be.eq("Not Found")
+            
+        })
     })
 })
