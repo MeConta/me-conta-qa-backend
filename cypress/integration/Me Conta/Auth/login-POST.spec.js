6fca3d97f74f65ace2cb8d129df742d7f48ff75a
barbaraest <barbara.esteves@thoughtworks.com>
2021-10-11 12:14:43 -0300

    ajuste login

--- cypress/integration/Me Conta/Auth/login-POST.spec.js
+++ cypress/integration/Me Conta/Auth/login-POST.spec.js
@@ -1,8 +1,28 @@
 /// <reference types="Cypress">
 
 describe('Me Conta ? - Auth',()=>{
+    it('POST - Login - Logado com sucesso',()=>{
+        cy.request({
+            method: 'POST',
+            url:'https://me-conta-backend.herokuapp.com/auth/login',
+            failOnStatusCode: false,
+            headers:{
+                "accept" : 'application/json',
+                "Content-Type" : 'application/json'
+            },
+            body:{
+                "username": "teste@teste.com",
+                "password": "s#nh4Valida"
+            },
+        }).as('response')
 
-    it('POST-Login - Senha inválida',()=>{
+        cy.get('@response').then(res =>{
+            cy.expect(res.status).to.be.eq(200)
+            cy.expect(res.body.token).to.be.not.null
+        })
+    })
+
+    it('POST - Login - Senha inválida',()=>{
         cy.request({
             method: 'POST',
             url: 'https://me-conta-backend.herokuapp.com/auth/login',
@@ -22,27 +42,4 @@ describe('Me Conta ? - Auth',()=>{
             cy.expect(res.body.message).to.be.eq("Unauthorized")
         })
     })
-
-    it('POST-Login - Logado com sucesso',()=>{
-        cy.request({
-            method: 'POST',
-            url:'https://me-conta-backend.herokuapp.com/auth/login',
-            failOnStatusCode: false,
-            headers:{
-                "accept" : 'application/json',
-                "Content-Type" : 'application/json'
-            },
-            body:{
-                "username": "teste@teste.com",
-                "password": "s#nh4Valida"
-            },
-        }).as('response')
-
-        cy.get('@response').then(res =>{
-            cy.expect(res.status).to.be.eq(200)
-            cy.expect(res.body.token).to.be.not.null
-        })
-
-    })
-
 })
