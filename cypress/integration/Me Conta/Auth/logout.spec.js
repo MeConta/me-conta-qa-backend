68fcb0499dbf51df5f9a9924d5c24d60c608fcc5
Flavia Souza <flavias.git@gmail.com>
2022-07-06 13:21:51 -0300

    test: fixing sonar hotspot security issue

--- cypress/integration/Me Conta/Auth/logout.spec.js
+++ cypress/integration/Me Conta/Auth/logout.spec.js
@@ -1,49 +1,51 @@
 /// <reference types="Cypress"/>
 
-import { faker } from '@faker-js/faker'
+import { faker } from "@faker-js/faker";
 
-describe('Me Conta ? - Auth', () => {
-    const req = {
-        username: faker.internet.email(),
-        password: 's#nh4Valida',
-        nome: faker.name.findName()
-    }
+describe("Me Conta ? - Auth", () => {
+  const senhaValida = "s#nh4Valida";
 
-    before(() => {
-        const usuario = {
-            email: req.username,
-            senha: req.password,
-            nome: req.nome
-        }
-        cy.cadastroInicial(usuario);
-        cy.login(req.username, req.password);
+  const req = {
+    username: faker.internet.email(),
+    password: senhaValida,
+    nome: faker.name.findName(),
+  };
+
+  before(() => {
+    const usuario = {
+      email: req.username,
+      senha: req.password,
+      nome: req.nome,
+    };
+    cy.cadastroInicial(usuario);
+    cy.login(req.username, req.password);
+  });
+
+  it("POST - Logout - Sair com sucesso", () => {
+    cy.request({
+      method: "POST",
+      url: "/auth/logout",
+      headers: {
+        Authorization: `Bearer ${Cypress.env("token")}`,
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).should(({ status }) => {
+      expect(status).to.be.eq(200);
     });
+  });
 
-    it('POST - Logout - Sair com sucesso', () => {
-        cy.request({
-            method: 'POST',
-            url: '/auth/logout',
-            headers: {
-                Authorization: `Bearer ${Cypress.env('token')}`,
-                "accept": 'application/json',
-                "Content-Type": 'application/json'
-            }
-        }).should(({ status }) => {
-            expect(status).to.be.eq(200);
-        });
-    })
-
-    it('POST - Logout - Unauthorized', () => {
-        cy.request({
-            method: 'POST',
-            url: '/auth/logout',
-            failOnStatusCode: false,
-            headers: {
-                "accept": 'application/json',
-                "Content-Type": 'application/json'
-            }
-        }).should(({ status }) => {
-            expect(status).to.be.eq(401);
-        });
-    })
+  it("POST - Logout - Unauthorized", () => {
+    cy.request({
+      method: "POST",
+      url: "/auth/logout",
+      failOnStatusCode: false,
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).should(({ status }) => {
+      expect(status).to.be.eq(401);
+    });
+  });
 });
